278f4bb04f765b57b75767d35349a3f2767a5601
abdurrakibrafi <aboudurrakib469@gmail.comm>
2023-08-30 15:31:18 +0600

    api integrated using model class

---
+++ lib/models/models_class.dart
@@ -0,0 +1,7 @@
+class Photos {
+  String title;
+  int id;
+  String body;
+
+  Photos({required this.title, required this.body, required this.id});
+}
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:rest_api_basic/models/models_class.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,8 +12,63 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Photos> photosList = [];
+  Future<List<Photos>> getPhotos() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        Photos photos = Photos(title: i['title'], body: i['body'], id: i['id']);
+        photosList.add(photos);
+      }
+      return photosList;
+    } else {
+      return photosList;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('API Integration'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+                future: getPhotos(),
+                builder: (context, AsyncSnapshot<List<Photos>> snapshot) {
+                  return ListView.builder(
+                      itemCount: photosList.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Card(
+                              child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('ID:' +
+                                    snapshot.data![index].id.toString()),
+                                Text('Title :' +
+                                    snapshot.data![index].title.toString()),
+                                Text('Body :' +
+                                    snapshot.data![index].body.toString())
+                              ],
+                            ),
+                          )),
+                        );
+                      });
+                }),
+          )
+        ],
+      ),
+    );
   }
 }
